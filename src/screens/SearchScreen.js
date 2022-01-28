73383dbe1893fe260de07d21c1d4bc26606c5a6e
Veselin <vesselin60@gmail.com>
2022-01-28 08:20:20 +0200

    Running an initial(default) search

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Text, StyleSheet, View, Button, TouchableOpacity} from 'react-native';
 
 import SearchBar from '../components/SearchBar';
@@ -9,12 +9,12 @@ const SearchScreen = ({navigation}) => {
   const [results, setResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const searchApi = async () => {
+  const searchApi = async searchTerm => {
     try {
       const response = await yelp.get('/search', {
         params: {
           limit: 50,
-          term,
+          term: searchTerm,
           location: 'san jose',
         },
       });
@@ -24,9 +24,17 @@ const SearchScreen = ({navigation}) => {
     }
   };
 
+  useEffect(() => {
+    searchApi('pasta');
+  }, []);
+
   return (
     <View>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
       <Button
