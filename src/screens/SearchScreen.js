3a741f5404cbadcd37d16bdee561739e35530abb
Veselin <vesselin60@gmail.com>
2022-01-28 10:40:30 +0200

    Hiding scroll bars

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
-import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
+import { Text, StyleSheet, View, Button, ScrollView } from 'react-native';
 
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
-import ResultsList from '../components/ResultsList'; 
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = ({ navigation }) => {
 	const [term, setTerm] = useState('');
 	const [searchApi, results, errorMessage] = useResults();
 
-    const filterResultsByPrice = (price) => {
-        return results.filter(result => {
-            return result.price === price;
-        });
-    };
+	const filterResultsByPrice = price => {
+		return results.filter(result => {
+			return result.price === price;
+		});
+	};
 
 	return (
 		<View>
@@ -24,14 +24,28 @@ const SearchScreen = ({ navigation }) => {
 			/>
 			{errorMessage ? <Text>{errorMessage}</Text> : null}
 			<Text>We have found {results.length} results</Text>
-            <ResultsList results={filterResultsByPrice('$')} title='Cost Effective' />
-            <ResultsList results={filterResultsByPrice('$$')} title='Bit Pricier' />
-            <ResultsList results={filterResultsByPrice('$$$')} title='Big Spender' />
-            <ResultsList results={filterResultsByPrice('$$$$')} title='Rockefeller' />
-			<Button
-				title="Go to Details Screen"
-				onPress={() => navigation.navigate('Details')}
-			/>
+			<ScrollView>
+				<ResultsList
+					results={filterResultsByPrice('$')}
+					title="Cost Effective"
+				/>
+				<ResultsList
+					results={filterResultsByPrice('$$')}
+					title="Bit Pricier"
+				/>
+				<ResultsList
+					results={filterResultsByPrice('$$$')}
+					title="Big Spender"
+				/>
+				<ResultsList
+					results={filterResultsByPrice('$$$$')}
+					title="Rockefeller"
+				/>
+				<Button
+					title="Go to Details Screen"
+					onPress={() => navigation.navigate('Details')}
+				/>
+			</ScrollView>
 		</View>
 	);
 };
