8f428fe2deebf26e4b9a032834c8c8441f7a1339
Veselin <vesselin60@gmail.com>
2022-01-28 07:56:19 +0200

    Error handling

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,18 +2,33 @@ import React, {useState} from 'react';
 import {Text, StyleSheet, View, Button, TouchableOpacity} from 'react-native';
 
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = ({navigation}) => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term,
+          location: 'san jose',
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage('Something went wrong');
+    }
+  };
 
   return (
     <View>
-      <SearchBar
-        term={term}
-        onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={() => console.log('term was submitted')}
-      />
-      <Text>{term}</Text>
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results</Text>
       <Button
         title="Go to Details Screen"
         onPress={() => navigation.navigate('Details')}
