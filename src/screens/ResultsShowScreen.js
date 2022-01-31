025da33916941713013bbd7d3f387b655214c710
Veselin <vesselin60@gmail.com>
2022-01-31 09:32:25 +0200

    Fetch data about a single restaurant

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import yelp from '../api/yelp';
 
-const ResultsShowScreen = ({route}) => {
-    const {id} = route.params;
+const ResultsShowScreen = ({ route }) => {
+	const [result, setResult] = useState(null);
+	const { id } = route.params;
 
-    console.log(id);
-    return (
-        <View>
-            <Text>Results Show Screen</Text>
-        </View>
-    );
-}; 
+    console.log(result);
+
+	const getResult = async id => {
+		const response = await yelp.get(`/${id}`);
+		setResult(response.data);
+	};
+
+    useEffect(() => {
+        getResult(id);
+    }, []);
+
+	return (
+		<View>
+			<Text>Results Show Screen</Text>
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({});
 
